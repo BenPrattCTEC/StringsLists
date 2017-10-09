78a7d731b57910a60e66ed014aebe8d8a025c53f
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-10-09 12:47:21 -0600

    added ability to remove kahoots from list, interactive creation, ability to search for kahoot by ID

--- src/stringsLists/controller/Controller.java
+++ src/stringsLists/controller/Controller.java
@@ -1,6 +1,8 @@
 package stringsLists.controller;
+
 import stringsLists.model.*;
 import stringsLists.view.*;
+import java.lang.IndexOutOfBoundsException;
 
 import java.util.ArrayList;
 
@@ -8,22 +10,77 @@ public class Controller {
 	
 	GuiController gui = new GuiController();
 	
-	public void start(){
+	ArrayList<Kahoot> kahoots;
+	
+	public void start() {
 		
-		ArrayList<Kahoot> kahoots = new ArrayList<Kahoot>();
+		kahoots = new ArrayList<Kahoot>();
+			
+		// Kahoot(String topic, String creator, int questionCount)
+		kahoots.add(new Kahoot("Math", "Ben", 10));
+		kahoots.add(new Kahoot("History", "Chris", 10));
+		kahoots.add(new Kahoot("English", "Tom", 34));
 		
-		//Kahoot(String topic, String creator, int questionCount)
-		kahoots.add(new Kahoot("Math","Ben", 10));
+		// create new kahoot
+		interactiveCreateKahoot();
 		
-		showKahoots(kahoots);
+		// remove a kahoot by ID
+		try {
+			if (!removeKahoot(Integer.parseInt(gui.displayQuestion(kahootListToString() + "\nEnter the ID of the kahoot to remove"))))
+				gui.displayText("Nothing was removed");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			gui.displayText("Nothing was removed");
+		}
+		
+		// display kahoots
+		gui.displayText(kahootListToString());
 		
 	}
 	
-	private void showKahoots(ArrayList<Kahoot> list){
+	private String kahootListToString() {
 		String buffer = "";
-		for(Kahoot i: list){
-			buffer+=i.toString() + "\n";
+		for (Kahoot i : kahoots) {
+			buffer += i.toString() + "\n\n";
 		}
-		gui.displayText(buffer);
+		return buffer;
 	}
+	
+	private boolean removeKahoot(int ID) {
+		try {
+			kahoots.remove(findIndexByID(ID));
+			return true;
+		}
+		catch (IndexOutOfBoundsException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	private void interactiveCreateKahoot() {
+		String topic = gui.displayQuestion("Enter Topic: ");
+		String creator = gui.displayQuestion("Enter Author: ");
+		int numOfQuestions = 0;
+		while (numOfQuestions <= 0) {
+			try {
+				numOfQuestions = Integer.parseInt(gui.displayQuestion("Enter Number of Questions: "));
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				gui.displayText("Invalid number");
+			}
+		}
+		kahoots.add(new Kahoot(topic==null? "":topic, creator==null? "":creator, numOfQuestions));
+	}
+	
+	private int findIndexByID(int ID) {
+		for (int i = 0; i <= kahoots.size(); i++) {
+			if (kahoots.get(i).getID() == ID) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
 }
